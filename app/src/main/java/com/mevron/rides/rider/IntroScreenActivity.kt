fb24f0859fcbbe48fa8e4d3bd9b0fad06e7b0912
Promise Ochornma <ochornma.promise@giglogistics.ng>
2022-01-13 11:03:04 +0100

    added dependencies

---
+++ app/src/main/java/com/mevron/rides/rider/IntroScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.mevron.rides.rider
+
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class IntroScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_screen)
+    }
+}
