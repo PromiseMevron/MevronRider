0faf123b910913f2f428c743ea9a5224065eb8b5
Promise Ochornma <promise@investbamboo.com>
2022-08-31 19:30:37 +0100

    fix app messaging error token

--- app/src/main/java/com/mevron/rides/rider/App.kt
+++ app/src/main/java/com/mevron/rides/rider/App.kt
@@ -3,7 +3,10 @@ package com.mevron.rides.rider
 import android.app.Application
 import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import dagger.hilt.android.HiltAndroidApp
+
 
 @HiltAndroidApp
 class App: Application() {
@@ -15,6 +18,12 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         ApplicationContext = this
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        val options = FirebaseOptions.Builder()
+            .setApplicationId("1:210013538605:android:570d8f4733e69b182b6ba4") // Required for Analytics.
+            .setProjectId("mevron-1330b") // Required for Firebase Installations.
+            .setApiKey("AIzaSyBJIQiM4tcpqcmnN4II6UHep9OJ025zsA8") // Required for Auth.
+            .build()
+        FirebaseApp.initializeApp(this, options, "Mevron Rider")
     }
 }
